5e9fc062d2edafb8e2eb39578650e2f9a60c2e6b
mab <mab@mab.com>
2023-10-19 10:46:13 +0500

    API Base URL Changed

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = "https://app-vcb7oopywq-de.a.run.app";
+export const API_BASE_URL = "https://backend-vcb7oopywq-uc.a.run.app";
 export const MAX_RESULTS = 10;
 export const DEFAULT_LANGUAGE = "en";
